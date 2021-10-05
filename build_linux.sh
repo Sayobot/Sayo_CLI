1e61e203b9e314c0a97a55ffdbb9693d5f82dab0
Sayobot <Sayobot@qq.com>
2021-10-05 11:20:37 +0800

    'update'

--- build_linux.sh
+++ build_linux.sh
@@ -2,15 +2,15 @@
 
 g++ --std=c++17 \
   -O2 -DRELEASE -DNDEBUG \
-  -I./jsoncpp-master/include \
-  -I./hidapi-master/hidapi \
+  -I./jsoncpp/include \
+  -I./hidapi/hidapi \
   -I./inc \
   o2_protocol.cpp \
   main.cpp \
   src/http.cpp \
   src/tools.cpp \
   -I/usr/include \
-  ./jsoncpp-master/bulid/lib/libjsoncpp.a \
+  ./jsoncpp/bulid/lib/libjsoncpp.a \
   -lhidapi-hidraw \
   -lpthread \
   -o Sayo_CLI_Linux
