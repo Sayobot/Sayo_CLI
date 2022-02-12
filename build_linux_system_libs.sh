e207b170421eba509780a89a5627ea062f65554d
Michal Lebeda <MichalLebeda@users.noreply.github.com>
2022-02-12 13:53:05 +0100

    Add Linux build script which uses system libs

---
+++ build_linux_system_libs.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+g++ --std=c++17 \
+  -O2 -DRELEASE -DNDEBUG \
+  -I/usr/include/json \
+  -I/usr/include/hidapi \
+  -I./inc \
+  o2_protocol.cpp \
+  main.cpp \
+  src/http.cpp \
+  src/tools.cpp \
+  -I/usr/include \
+  -ljsoncpp \
+  -lhidapi-hidraw \
+  -lpthread \
+  -o Sayo_CLI_Linux
+
